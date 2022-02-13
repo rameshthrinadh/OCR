65a59cf44b648a0b9dfca71a6563ca7567614198
rameshthrinadh <yrtrinadh@gmail.com>
2022-02-13 11:04:10 +0530

    first commit

---
+++ OCR/Pocket-Ocr-Sever/src/utils/cloudinary.ts
@@ -0,0 +1,26 @@
+import * as cloudinary_import from 'cloudinary';
+import DataURIParser from 'datauri/parser';
+import path from 'path';
+import { Request } from 'express';
+
+const parser = new DataURIParser();
+
+const cloudinary = cloudinary_import.v2;
+cloudinary.config({ 
+    cloud_name: 'dsnrm77jx', 
+    api_key: '431681916459295', 
+    api_secret: 'pogFleLHMFPtMm3IueFr55pehfQ' 
+  });
+
+const dUri = (req: Request) => {
+    return parser.format(
+        path.extname(req.file!.originalname).toString(),
+        req.file!.buffer
+    );
+};
+
+const dUriPdf = (buffer: Buffer) => {
+    return parser.format('.pdf', buffer);
+};
+
+export { cloudinary, dUri, dUriPdf };
